90c0b2833d967f6843f23fe1a500321a0d32c1cb
Raphael Karanja <rkaranja@cytonn.com>
2018-11-26 12:26:57 +0300

    add boiler plate for card component and render it to app component

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,16 @@
 /**
  * Created by Raphael Karanja on 26/11/2018.
  */
+import React from 'react';
+const Card = (props)=>{
+    return (
+        <div>
+            <img src="http://placehold.it/75" alt=""/>
+            <div>
+                <div>Name Here...</div>
+                <div>Company Name here...</div>
+            </div>
+        </div>
+    )
+};
+export default Card;
